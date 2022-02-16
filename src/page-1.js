e980989404094701b0092bf09cf59cb7eef88c21
Lucas <novo.contato.lucas@gmail.com>
2022-02-15 23:50:15 -0300

    renderizando os quizzes na tela

--- src/page-1.js
+++ src/page-1.js
@@ -1,14 +1,31 @@
 function GetAllQuizzes() {
+    
     const promise = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes')
     promise.then((response) => {
         console.log(response.data)
+        RenderAllQuizzes(response.data)
     })
     promise.catch((error) => {
         console.error(error)
     })
 }
 
+function RenderAllQuizzes(quizzes) {
 
+    const allquizzcontainer = document.querySelector('.all-quizz-container')
+    allquizzcontainer.innerHTML = ''
+
+    for (let i = 0; i < quizzes.length; i++) {
+        // console.log(quizzes[i].id, quizzes[i].title, quizzes[i].image)
+        allquizzcontainer.innerHTML += `
+            <div class="all-quizz-box" id = "${quizzes[i].id}">
+                <h1 class = 'title'>${quizzes[i].title}</h1>
+            </div>
+        `
+        const allquizzbox = document.getElementById(`${quizzes[i].id}`)
+        allquizzbox.style.backgroundImage = `url('${quizzes[i].image}')`
+    }
+}
 
 
 // exportando a função
