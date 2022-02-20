e1c1683a459356659229631fe97c0a4fbec326f9
Lucas <novo.contato.lucas@gmail.com>
2022-02-20 05:12:15 -0300

    falta apenas criar o objeto dinamicamente e o bonus

--- src/page-1.js
+++ src/page-1.js
@@ -1,9 +1,11 @@
+let TOTAL_USER_QUIZZ = 0
+
 window.onload = () => {
-    getDataQuizz()
-    //GetAllQuizzes()
+
+    GetAllQuizzes()
+    CheckUserLocalStorage()
 }
-let quizzTitle;
-let quizzUrl;
+
 function GetAllQuizzes() {
 
     const promise = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes')
@@ -20,8 +22,8 @@ function RenderAllQuizzes(quizzes) {
 
     const allquizzcontainer = document.querySelector('.all-quizz-container')
     allquizzcontainer.innerHTML = ''
-    
-     
+    allquizzcontainer.innerHTML = '<div class = "all-quizz-container-p"><p>Todos os Quizzes</p></div>'
+
     for (let i = 0; i < quizzes.length; i++) {
         allquizzcontainer.innerHTML += `
             <div class="all-quizz-box" id = "${quizzes[i].id}" onclick="getOnlyQuizz(${quizzes[i].id})">
@@ -37,6 +39,7 @@ let currentPage = 1
 let numberOfQuestions = 0
 let ValidateUserQuizz = false
 let numberOfLevels = 0
+let GetThisQuizzID_ = undefined
 
 function GetUserQuizzData(page) {
 
@@ -55,7 +58,7 @@ function GetUserQuizzData(page) {
     }
     if (page === 'questions') {
 
-         quizzTitle = document.querySelector('.user-quizz-title').value
+        quizzTitle = document.querySelector('.user-quizz-title').value
         quizzUrl = document.querySelector('.user-quizz-url').value
         let quizzQuestionsAmount = document.querySelector('.user-quizz-questions-amount').value
         let quizzLevelsAmount = document.querySelector('.user-quizz-levels-amount').value
@@ -92,20 +95,24 @@ function GetUserQuizzData(page) {
         if (ValidateUserQuizzLevel) {
             quizzLevels.classList.add('hidden')
             quizzSuccess.classList.remove('hidden')
-            CreateNewQuizz();
+
+            // ao criar um quizz, o armazenamento local do usuario tera essa informacao
+            window.localStorage.setItem('UserCreateQuizz', 'true');
+            GenerateUserRequestPost(numberOfQuestions, numberOfLevels)
         }
     }
     if (page === 'acess') {
         page3.classList.add('hidden')
         page2.classList.remove('hidden')
 
-         getOnlyQuizz(id)
+        if (GetThisQuizzID_ != undefined) {
+            getOnlyQuizz(GetThisQuizzID_)
+        }
     }
     if (page === 'home') {
         page3.classList.add('hidden')
         quizzSuccess.classList.add('hidden')
         page1.classList.remove('hidden')
-        getDataQuizz();
     }
 }
 
@@ -130,16 +137,16 @@ function RenderQuestions(number, page) {
     for (let i = 1; i <= number; i++) {
         if (page !== i) {
             QuestionsInputContainer.innerHTML += `
-                <div class="quizz-question-box" id = '${i}'>
+            <div class="quizz-question-box" id = '${i}'>
                 <div class="text"> <h1>Pergunta ${i}</h1></div>
                 <div class="icon" onclick="EditThisQuestion(${i})"><ion-icon name="create-outline"></ion-icon></div>
-                </div>`
+            </div>`
         }
     }
     QuestionsInputContainer.innerHTML += `
-        <div class="quizz-question-final-btn">
-            <button onclick="GetUserQuizzData('levels')">Prosseguir pra criar níveis</button>
-        </div>`
+    <div class="quizz-question-final-btn">
+        <button onclick="GetUserQuizzData('levels')">Prosseguir pra criar níveis</button>
+    </div>`
 }
 
 function CheckInfoQuizzData(title, url, questions, levels) {
@@ -158,17 +165,12 @@ function CheckInfoQuizzData(title, url, questions, levels) {
         return alert('A quantidade de niveis deve ser no mínimo 2')
     }
     numberOfLevels = levels
-    console.log(numberOfLevels)
     return true
 }
 
 let GetFormData = []
 
 function EditThisQuestion(number) {
-
-    // if(number === numberOfQuestions) {
-    //     console.log('estou editando a ultima questao')
-    // }
 
     let quizzQuestion = document.getElementById('p1').value
     let quizzQuestionColor = document.getElementById('p2').value
@@ -249,21 +251,21 @@ function RenderLevels(page, number) {
         if (page !== i) {
             LevelsInputContainer.innerHTML += `
             <div class="quizz-levels-box" id = '${i}'>
-            <div class="text"> <h1>Nivel ${i}</h1></div>
-            <div class="icon" onclick="EditThisLevel(${i})"><ion-icon name="create-outline"></ion-icon></div>
+                <div class="text"> <h1>Nivel ${i}</h1></div>
+                <div class="icon" onclick="EditThisLevel(${i})"><ion-icon name="create-outline"></ion-icon></div>
             </div>`
         }
     }
     LevelsInputContainer.innerHTML += `
-        <div class="quizz-levels-final-btn"><button onclick="GetUserQuizzData('success')">Finalizar Quizz</button></div>`
+    <div class="quizz-levels-final-btn"><button onclick="GetUserQuizzData('success')">Finalizar Quizz</button></div>`
 }
 
 let GetFormLevel = []
 
 function EditThisLevel(number) {
+
     let quizzLevel = document.getElementById('x1').value
     let LevelPorcent = document.getElementById('x2').value
-
     let LevelURL = document.getElementById('x3').value
     let LevelDescription = document.getElementById('x4').value
 
@@ -284,7 +286,6 @@ function EditThisLevel(number) {
 let ValidateUserQuizzLevel = false
 
 function CheckLevelQuizzData(page, array) {
-    console.log('funcao CheckLevelQuizzData call')
     if (array[page] === null || array[page] === undefined) {
         alert('Voce precisa preencher as informacoes de todas os niveis antes de continuar')
         return false
@@ -311,6 +312,198 @@ function CheckLevelQuizzData(page, array) {
 
 const CheckURL = (url) => url.includes('http://') || url.includes('https://') ? true : false
 
-// commit debug
+function CheckUserLocalStorage() {
 
+    let check = window.localStorage.getItem('UserCreateQuizz')
+    if (check === undefined || check === null || check === 'false') {
+        console.log('Voce nao criou um quizz ainda')
+        TOTAL_USER_QUIZZ = 0
+    }
+    else {
+        TOTAL_USER_QUIZZ = window.localStorage.getItem('TOTAL_USER_QUIZZ')
+        console.log(`Voce possui um total de ${TOTAL_USER_QUIZZ} quizzes `)
 
+        let AllUserQuizz = []
+        for (let i = 1; i <= TOTAL_USER_QUIZZ; i++) {
+            AllUserQuizz[i] = window.localStorage.getItem(`UserQuizzID${i}`);
+
+            console.log(`O quizz do indice ${i} foi renderizdo`)
+            RenderUserQuizz(AllUserQuizz[i])
+        }
+    }
+}
+
+function GenerateUserRequestPost(questions, levels) {
+
+    // infelizmente o objeto gerado por esse metodo esta causando um erro na API
+    // temos que consertar 
+    // ------------------------------------------- >>
+    let arrayQuestions = []
+    let arrayLevels = []
+    let userQuizzOBJ = {}
+
+    for (let i = 0; i < questions; i++) {
+        arrayQuestions.push(createNewQuestion(i));
+    }
+    for (let i = 0; i < levels; i++) {
+        arrayLevels.push(createNewLevel(i));
+    }
+
+    userQuizzOBJ.questions = arrayQuestions
+    userQuizzOBJ.levels = arrayLevels
+
+    // let requestUserQuizz = axios.post('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes', userQuizzOBJ)
+    // ------------------------------------------- >>
+
+    // observe que eu passei o objeto (testes) na API
+    let requestUserQuizz = axios.post('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes', testes)
+
+    requestUserQuizz.then((response) => {
+        TOTAL_USER_QUIZZ++
+        window.localStorage.setItem(`UserQuizzID${TOTAL_USER_QUIZZ}`, `${response.data.id}`)
+        window.localStorage.setItem('TOTAL_USER_QUIZZ', `${TOTAL_USER_QUIZZ}`)
+        console.log(`voce criou o quizz id ${response.data.id} com extremo sucesso`)
+
+        // obtendo o ID do Quizz criado pra poder acessa-lo
+        GetThisQuizzID_ = response.data.id
+        RenderUserQuizz(response.data.id)
+    })
+}
+
+function RenderUserQuizz(idQuizz) {
+
+    let responseQuizz = axios.get(`https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/${idQuizz}`)
+    responseQuizz.then((response) => {
+
+        // console.log(response.data.title) ok
+        let GetTitle_ = response.data.title
+
+        document.querySelector('.user-quizz-container').classList.remove("hidden")
+        document.querySelector('.create-quizz-container').classList.add("hidden")
+
+        console.log(`quizz ${idQuizz} renderizou`)
+
+        document.querySelector('.user-quizz-container').innerHTML += `
+        <div class="all-quizz-box" id = "${idQuizz}" onclick="getOnlyQuizz(${idQuizz})">
+        <h1 class = 'title'>${GetTitle_}</h1> </div>`
+    })
+}
+
+// esse objeto eh o mesmo usado no exemplo do site da API
+// passando esse objeto da certo !
+// precisamos encontrar uma forma de passar esse objeto dinamicamente
+let testes = {
+    title: "Deu certo ! :)",
+    image: "https://cidadeolimpica.com.br/wp-content/uploads/2020/09/papel-parede-bahia-pc7-1024x613.jpeg",
+    questions: [
+        {
+            title: "Título da pergunta 1",
+            color: "#123456",
+            answers: [
+                {
+                    text: "Texto da resposta 1",
+                    image: "https://http.cat/411.jpg",
+                    isCorrectAnswer: true
+                },
+                {
+                    text: "Texto da resposta 2",
+                    image: "https://http.cat/412.jpg",
+                    isCorrectAnswer: false
+                }
+            ]
+        },
+        {
+            title: "Título da pergunta 2",
+            color: "#123456",
+            answers: [
+                {
+                    text: "Texto da resposta 1",
+                    image: "https://http.cat/411.jpg",
+                    isCorrectAnswer: true
+                },
+                {
+                    text: "Texto da resposta 2",
+                    image: "https://http.cat/412.jpg",
+                    isCorrectAnswer: false
+                }
+            ]
+        },
+        {
+            title: "Título da pergunta 3",
+            color: "#123456",
+            answers: [
+                {
+                    text: "Texto da resposta 1",
+                    image: "https://http.cat/411.jpg",
+                    isCorrectAnswer: true
+                },
+                {
+                    text: "Texto da resposta 2",
+                    image: "https://http.cat/412.jpg",
+                    isCorrectAnswer: false
+                }
+            ]
+        }
+    ],
+    levels: [
+        {
+            title: "Título do nível 1",
+            image: "https://http.cat/411.jpg",
+            text: "Descrição do nível 1",
+            minValue: 0
+        },
+        {
+            title: "Título do nível 2",
+            image: "https://http.cat/412.jpg",
+            text: "Descrição do nível 2",
+            minValue: 50
+        }
+    ]
+}
+
+// esssa funcao esta criando o array dinamicamente, porem ao passar o objeto na API ela retorna um erro 422
+// ou seja, a API nao esta conseguindo processar os dados. Precisamos verificar se o objeto gerado por esse metodo
+// e igual ao objeto que a API retorna na lista de quizzes
+
+function createNewQuestion(index) {
+    const newDataQuestion = {
+        "title": GetFormData[index].quizzQuestion,
+        "color": GetFormData[index].quizzQuestionColor,
+        "answers": [
+            {
+                "text": GetFormData[index].quizzRightAnswer,
+                "image": GetFormData[index].quizzRightAnswerURL,
+                "isCorrectAnswer": true
+            },
+            {
+                "text": GetFormData[index].quizzIncorrectAnswer1,
+                "image": GetFormData[index].quizzIncorrectAnswerURL1,
+                "isCorrectAnswer": false
+            }
+        ]
+    }
+    if (GetFormData[index].quizzIncorrectAnswer2 != "") {
+        newDataQuestion.answers.push({
+            "text": GetFormData[index].quizzIncorrectAnswer2,
+            "image": GetFormData[index].quizzIncorrectAnswerURL3,
+            "isCorrectAnswer": false
+        })
+    }
+    if (GetFormData[index].quizzIncorrectAnswer3 != "") {
+        newDataQuestion.answers.push({
+            "text": GetFormData[index].quizzIncorrectAnswer3,
+            "image": GetFormData[index].quizzIncorrectAnswerURL3,
+            "isCorrectAnswer": false
+        })
+    }
+    return newDataQuestion;
+}
+function createNewLevel(index) {
+    const newDataLevel = {
+        "title": GetFormLevel[index].quizzLevel,
+        "minValue": GetFormLevel[index].LevelPorcent,
+        "image": GetFormLevel[index].LevelURL,
+        "text": GetFormLevel[index].LevelDescription
+    }
+    return newDataLevel;
+}
